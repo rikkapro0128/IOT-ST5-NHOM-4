37b228a8cfa9b42949a8841c3602038fadf194c1
rikapro0128 <stkssc002@gmail.com>
2022-04-25 03:40:10 +0700

    save code

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-$(function() {
+$(function () {
   // Handler for .ready() called.
-  
+  $('.state--node').click(function() {
+    $('.miru--modal__raspberry').modal('show');
+  })
+  for (const ele of $('.miru--checkbox > .behavior')) {
+    $(ele).click(handleCheckBox);
+  }
 });
+
+function handleCheckBox(e) {
+  if ($(e.target).hasClass('active')) {
+    $(e.target).removeClass('active')
+    $(e.target).siblings('.label').text('tắt')
+  } else {
+    $(e.target).addClass('active')
+    $(e.target).siblings('.label').text('mở')
+  }
+}
