230c196b9759cdee8043e0885260539266545d3b
rikapro0128 <stkssc002@gmail.com>
2022-04-30 02:20:20 +0700

    save code

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-
 $(function () {
   // Handler for .ready() called.
   // global variable 
   const numberSecondWaits = 15 * 1000;
+  const messageDisableByWait = 'bạn có thể nhấn sau';
+  const timeShowToast = 3000;
+  const TITLE = {
+    switch: {
+      on: 'bật',
+      off: 'tắt',
+    },
+    touch: {
+      on: 'chạm',
+      off: 'không chạm',
+    },
+    smoke: {
+      detach: 'có khói',
+      un_detach: 'không khói',
+    },
+  };
   const manageDevice = [
     {
       field: 'light',
       id: 1716053,
       type: 'digital',
       wait: false,
+      timeWaitRemain: 0,
       api_key: '0OBH7KJFUZ34STBY',
     },
     {
@@ -16,6 +32,7 @@ $(function () {
       id: 1716052,
       type: 'digital',
       wait: false,
+      timeWaitRemain: 0,
       api_key: 'JAQHPWZTHOO8ENA8',
     },
     {
@@ -23,6 +40,7 @@ $(function () {
       id: 1716054,
       type: 'hex',
       wait: false,
+      timeWaitRemain: 0,
       api_key: 'BFG26RKKLXT4CJ0Q',
     },
   ]
@@ -46,40 +64,71 @@ $(function () {
     $(ele).click(handleCheckBox);
   }
 
+  function showToast(message = 'no message!', type = 'notify', time = -1) {
+    if($(window).width() > 786) {
+      Toastify({
+        text: message,
+        className: `miru--toast miru--${type}`,
+        offset: {
+          x: '3rem', // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+          y: '3rem' // vertical axis - can be a number or a string indicating unity. eg: '2em'
+        },
+        duration: -1,
+        close: true,
+        gravity: 'bottom',
+        position: 'right',
+        backgroundColor: '#fff',
+        duration: time,
+        avatar: `${type === 'notify' ? 'assets/images/notification-pngrepo-com.png' : (type === 'error' ? 'assets/images/error-pngrepo-com.png' : 'assets/images/warning-pngrepo-com.png')}`,
+      }).showToast();
+    }else {
+      $('.miru--modal__toast').find('.title').text(message);
+      $('.miru--modal__toast').modal('show');
+    }
+  }
 
   function handleCheckBox(e) {
 
     const ele = $(e.target);
-    let indexItem;
-    const item = manageDevice.find((value, index) => { if (value.field === ele.closest('.miru--checkbox').attr('data-field')) { indexItem = index; return true; } });
-
-    if (item.api_key) {
+    let indexDevice;
+    const item = manageDevice.find((value, index) => { if (value.field === ele.closest('.miru--checkbox').attr('data-field')) { indexDevice = index; return true; } });
+    // console.log(item);
+    if (item.api_key && !item.wait) {
       if (ele.hasClass('active')) {
-        setValueByChannle(item.api_key, { field1: 0 })
+        setValueByChannle(item.api_key, { field1: 0 }, indexDevice)
           .then((data) => {
             if (data !== 0) {
               ele.removeClass('active');
               ele.siblings('.label').text('tắt');
             } else {
-              console.log('show toast!');
+              showToast(`không thể thay đổi trạng thái thiết bị ${item.field}`, 'error', timeShowToast);
             }
           })
       } else {
-        setValueByChannle(item.api_key, { field1: 1 })
+        setValueByChannle(item.api_key, { field1: 1 }, indexDevice)
           .then((data) => {
             if (data !== 0) {
               ele.addClass('active');
               ele.siblings('.label').text('mở');
             } else {
-              console.log('show toast!');
+              showToast(`không thể thay đổi trạng thái thiết bị ${item.field}`, 'error', timeShowToast);
             }
           })
       }
+    } else {
+      showToast(`${messageDisableByWait} ${item.timeWaitRemain / 1000}s`, 'warming', timeShowToast);
     }
 
   }
 
-  function setValueByChannle(api_key, value) {
+  function setValueByChannle(api_key, value, indexDevice) {
+    manageDevice[indexDevice].wait = true;
+    manageDevice[indexDevice].timeWaitRemain = numberSecondWaits;
+    let stack = setInterval(() => { manageDevice[indexDevice].timeWaitRemain -= 1000 }, 1000);
+    setTimeout(() => {
+      manageDevice[indexDevice].wait = false;
+      clearInterval(stack);
+    }, numberSecondWaits)
     return new Promise((res, rej) => {
       fetch(`https://api.thingspeak.com/update.json`, {
         method: 'POST',
@@ -113,15 +162,14 @@ $(function () {
       getValueByChannle(manageSensor.idChannle, 'muti')
         .then(data => {
           for (const field of Object.entries(manageSensor.fields)) {
-            if(field[1] === 'smoke') {
-              $(`.${field[1]}.sensor__nav--item`).find('.nav--center--val').first().text(data[field[0]] ? 'có khói' : 'không khói' );
-            }else if(field[1] === 'touch') {
-              $(`.${field[1]}.sensor__nav--item`).find('.nav--center--val').first().text(data[field[0]] ? 'chạm' : 'không chạm' );
-            }else {
+            if (field[1] === 'smoke') {
+              $(`.${field[1]}.sensor__nav--item`).find('.nav--center--val').first().text(data[field[0]] ? TITLE.smoke.detach : TITLE.smoke.un_detach);
+            } else if (field[1] === 'touch') {
+              $(`.${field[1]}.sensor__nav--item`).find('.nav--center--val').first().text(data[field[0]] ? TITLE.touch.on : TITLE.touch.off);
+            } else {
               $(`.${field[1]}.sensor__nav--item`).find('.nav--center--val').first().text(data[field[0]]);
             }
           }
-          // $(`.${manageSensor[field1]}.sensor__nav--item`)
         })
     }, numberSecondWaits / 3);
 
@@ -132,15 +180,16 @@ $(function () {
     Promise.all(queu)
       .then(data => {
         data.forEach((value, index) => {
-          // manageDevice[index].wait = true;
-          // setTimeout(() => manageDevice[index].wait = false, numberSecondWaits)
+          const el = $(`.miru--checkbox[data-field=${manageDevice[index].field}] > .behavior`);
           if (value.field1 === '-1' || value.field1 === '0') {
             if (manageDevice[index].type === 'digital') {
-              $(`.miru--checkbox[data-field=${manageDevice[index].field}] > .behavior`).removeClass('active');
+              el.removeClass('active');
+              el.siblings('span.label').text(TITLE.switch.off);
             }
           } else {
             if (manageDevice[index].type === 'digital') {
-              $(`.miru--checkbox[data-field=${manageDevice[index].field}] > .behavior`).addClass('active');
+              el.addClass('active');
+              el.siblings('span.label').text(TITLE.switch.on);
             }
           }
         })
